6365efd0c37fc6d9fed96d5e4e92e6a26f40a178
swethakanala <swetha.bim@gmail.com>
2024-07-30 12:56:19 -0400

    terraform-vpc-test

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+# output "azs_info" {
+#   value = module.vpc.azs  # module.<module-name>.<output>
+# }
+
+output "vpc_id" {
+ value =  module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+ value =  module.vpc.public_subnet_ids
+}
